647be1268c4237fd85a8dfe01069dda36693ff93
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-04-26 19:30:52 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,42 @@
+require 'pry'
+class CashRegister
+attr_accessor :cash_register, :cash_register_with_discount, :total
 
+
+
+def initialize(discount = 0)
+@total = 0
+@discount = discount
+@item_list  = []
+end
+
+def discount
+@discount
+end
+
+def add_item(item, price, quantity = 1)
+      some_items_hash= {}
+      some_items_hash[:name] = item
+      some_items_hash[:price] = price
+      some_items_hash[:quantity] = quantity
+
+    @item_list <<  some_items_hash
+    @total += price * quantity
+end
+
+def apply_discount
+  if @discount == 0
+    return "There is no discount to apply."
+  elsif @discount > 1
+    @total -= @total * @discount / 100
+    return "After the discount, the total comes to $#{@total}."
+  end
+end
+
+def items
+  @item_list[name].each do |x|
+    x
+end
+end
+
+end
